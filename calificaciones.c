a824d0f91361572bd3d8b3f164464496ee0796ff
Usuario de la UCA <jdkdejava@gmail.com>
2017-03-13 19:20:37 +0100

    Añadidos archivos calificaciones.c y faltas.c, sin funcionalidad ninguno.

---
+++ calificaciones.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+//No tuve tiempo para hacer mas implementacion :P
+
+int main(void) {
+    
+    return 0;
+}
---
+++ faltas.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//No tuve tiempo para hacer mas implementacion :P
+
+int main(void) {
+    
+    return 0;
+}
+
+
