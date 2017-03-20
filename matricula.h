d9cff55170d6f98729da760b6ba16fd1a65a7010
rivera94 <sergiorivera196@gmail.com>
2017-03-20 18:18:59 +0100

    Primera versión del modulo matriculas

---
+++ matricula.h
@@ -0,0 +1,12 @@
+#ifndef MATRICULA_H
+#define MATRICULA_H
+#include "estructuras.h"
+
+
+//Cabecera: void mostrar(char idmateria, matriculas mat[], alumnos alum[])
+//Precondición: Estructura matriculas y alumnos creada, con todos los datos cargados.
+//Postcondición: Muestra todos los alumnos matriculados en una asignatura junto con sus datos.
+void mostrar(char idmateria, matriculas mat[], alumnos alum[]);
+
+#endif /* MATRICULA_H */
+
