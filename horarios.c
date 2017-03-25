d394fc2316cd15d041b1d4f9fc822e6f0b1f23fc
Guillermo Hendrix <Guillermo Hendrix>
2017-03-25 17:46:18 +0100

    Añadidos modulos main, y horarios. Revisad todos los comentarios en el código, y ponemos ideas en común el próimo lunes. Documentación acabada a falta de dos modulos.c, pocos detalles más y comenzar las pruebas de software.:

---
+++ horarios.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "horarios.h"
+#include "estructuras.h"
+
+/*
+   Cabecera: char *, horario *
+   Precondicion: vectores declarados e inicializados
+   Postcondicion: lee una linea y guardo todos sus atributos en una
+   estructura horario
+*/
+void leer_horario (char * linea, horario * h) {
+
+    if (linea == NULL) {
+      perror("Error: linea vacia.\n");
+    }
+
+    else {
+      strcpy(h->id_prof, strtok (linea,"-"));
+
+      strcpy(h->dia_clase, strtok (NULL,"-"));
+
+      strcpy(h->hora_clase, strtok (NULL,"-"));
+
+      strcpy(h->id_materia, strtok (NULL,"-"));
+
+      strcpy(h->grupo, strtok (NULL,"\n"));
+    }
+}
+
+/*
+	Cabecera: horario **
+	Precondición: puntero a vector de estructura declarado e inicializado
+	Postcondición: Lee todos los horarios que se encuetran en el fichero horarios.tt
+	y los guarda en un vector de punteros a estructura tipo horario
+*/ 
+int leer_horarios (horario ** arr) {
+
+int i = 0;
+char linea[MAX_LINEA];
+  FILE * fichero = fopen("horarios.txt", "r");
+
+  if (fichero == NULL)
+    exit(EXIT_FAILURE);
+	
+	//Reserva progresiva de memoria para almacenar cada
+	//horario en el vector
+  while (fgets (linea, sizeof linea, fichero)) {
+    arr[i] = malloc (sizeof (horario));
+    leer_horario(linea, arr[i]);
+    i++;
+  }
+
+  fclose(fichero);
+
+  return i;
+}
---
+++ main.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+//Minusculas y mayusculas en tus .c y .h, revísalo, Guille
+//Incompleta a falta de varios archivos.h, ¡¡REVISAR!!
+
+#include "estructuras.h"
+#include "ficheros.h"
+#include "alumnos.h"
+#include "matricula.h"
+#include "usuarios.h"
+
+
+//	Sección de importación (incompleta a falta de revisión)
+
+void menu_principal();
+void menu_admin();
+void menu_prof();
+void guardar();
+
+// Variables globales
+//Contadores de lineas de .txt
+//Si sobra o falta alguno añadir o eliminar
+
+int n_horarios, n_alumnos, n_usuarios;
+int n_materias, n_matriculas, n_faltas, n_calificaciones; 
+
+/* Proposición de hacer globales los vectores que
+	apuntan a una estructura con los datos de cada 
+	archivo .txt. Ej: 
+
+horario **arr_horario
+*/
+
+int main()
+{
+	printf("Cuaderno digital del profesor\n");
+
+	//Calcula el numero de lineas de cada fichero de texto
+	//Elminad los que os sobren
+
+	n_horarios = cuenta_lineas("horarios.txt");
+	n_alumnos = cuenta_lineas("alumnos.txt");
+	n_usuarios = cuenta_lineas("usuarios.txt");
+	n_materias = cuenta_lineas("materias.txt");
+	n_matriculas = cuenta_lineas("materias.txt");
+	n_faltas = cuenta_lineas("faltas.txt");
+	n_calificaciones = cuenta_lineas("calificaciones.txt");
+
+/*  
+	Reserva de memoria para los vectores que contendrán la
+	la base de datos del sistema
+*/ 
+//Así con todos haría yo...
+
+	arr_horario = malloc(n_horarios * sizeof(horario*));
+
+//Carga de ficheros en memoria
+// Así con todos
+
+	leer_horarios(arr_horario);
+
+	menu_principal();
+
+	return 0;
+}
+
+
+/*
+	Cabecera: ninguna
+	Precondición: ninguna
+	Postcondición: permite el login de los usuarios del sistema
+*/
+void menu_principal(){
+
+	
+}
