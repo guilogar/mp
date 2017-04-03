e39fd7f0d4d3ce7d0efffeb964ef148e52f447c0
Guillermo Hendrix <Guillermo Hendrix>
2017-04-03 17:45:33 +0200

    Algó pasó en el último commit y no se subión bien

--- horarios.c
+++ horarios.c
@@ -10,7 +10,7 @@
    Postcondicion: lee una linea y guardo todos sus atributos en una
    estructura horario
 */
-void leer_horario (char * linea, horario * h) {
+void leer_horario (char * linea, horarios * h) {
 
     if (linea == NULL) {
       perror("Error: linea vacia.\n");
@@ -35,7 +35,7 @@ void leer_horario (char * linea, horario * h) {
 	Postcondición: Lee todos los horarios que se encuetran en el fichero horarios.tt
 	y los guarda en un vector de punteros a estructura tipo horario
 */ 
-int leer_horarios (horario ** arr) {
+int leer_horarios (horarios ** arr) {
 
 int i = 0;
 char linea[MAX_LINEA];
@@ -63,6 +63,165 @@ char linea[MAX_LINEA];
 }
 
 
+/*
+  Cabecera: horario ** arr, int n, usuarios id
+  Precondición: vector que apunta a estructura inicializado
+  Postcondición: lista todos los horarios de un profesor
+*/
+void listar_horarios_prof (horarios ** arr, int *n, usuarios id) {
+  int i = 0, num= *num;
+  from (i; i<num; i++) {
+    if (strcmp(arr[i]->id_prof, usuarios id))
+    {
+      printf("Horarios\n=========================================================\n");
+      printf("%s-%d-%d-%s-%s\n", arr[i]->id_prof, arr[i]->dia_clase, arr[i]->hora_clase, arr[i]->id_materia, arr[i]->grupo );
+      printf("=========================================================\n");
+    }
+  }
+}
+
+/*
+  Cabecera: horario ** arr, int n, usuarios id
+  Precondición: vector que apunta a estructura inicializado
+  Postcondición: lista todos los horarios de un profesor
+*/
+void listar_horarios_admin (horarios ** arr, int *n) {
+  int i = 0, num = *n;
+  from (i; i<num; i++) {
+    printf("Horarios\n=========================================================\n");
+    printf("%s-%d-%d-%s-%s\n", arr[i]->id_prof, arr[i]->dia_clase, arr[i]->hora_clase, arr[i]->id_materia, arr[i]->grupo );
+    printf("=========================================================\n");
+  }
+}
+
+/*
+  Cabecera: horarios **arr_horarios, int n
+  Precondición: vector que apunta a estructuras inicializado, y nunero de datos contabilizado
+  Postcondición: Permite añadir un nuevo horario a un determinado usuario del sistema
+*/
+void añadir_horario (horarios **arr_horarios, int *n) {
+  int dia, hora, i, bol=1, num = *n;
+  char id_p[IDP], id_m[IDM], gr[GRUPO]; 
+  listar_horarios_admin(arr_horarios, n);
+
+  arr_horarios = realloc(arr_horarios, (num+1) * sizeof *horarios)
+  arr_horarios = malloc (sizeof(horarios));
+
+  num++;
+
+  printf("Introduzca el nuevo horario: \n");
+  while (bol != 0) {
+    printf("Id_prof: \n");
+    scanf("%s", id_p);
+    from(i=0; i<num-1; i++) {
+      if (strcmp (arr_horarios[num]->id_prof, id_p) == 0 )
+        bol = 0;
+    } 
+  }
+  printf("Introduzca día: \n");
+  scanf("%d", &dia);
+  printf("Introduzca hora: \n");
+  scanf("%d", &hora);
+  printf("Introduzca materia: \n");
+  scanf("%s", id_m);
+  printf("Introduzca grupo: \n");
+  scanf("%s", grupo);
+  strcpy(arr_horarios[num]->id_prof, id_p);
+  strcpy(arr_horarios[num]->id_materia, id_m);
+  strcpy(arr_horarios[num]->grupo, gr);
+  arr_horarios[num]->dia_clase = dia;
+  arr_horarios[num]->hora_clase = hora;
+  *n = num;
+}
+
+/*
+  Cabecera: horarios **arr_horarios, int n
+  Precondición: vector de punteros a estructura inicializado
+  Postcondición: elimina un horario de la lista.
+*/
+void eliminar_horario(horarios **arr_horarios, int *n) {
+
+int dia, hora, i, bol=1, num = *n;
+char id_p[IDP], id_m[IDM], gr[GRUPO]; 
+listar_horarios_admin(arr_horarios, num);
+
+while (bol != 0) {
+  printf("Introduzca identificador de profesor: \n");
+  scanf("%s", id_p);
+  printf("Introduzca día: \n");
+  scanf("%d", &dia);
+  printf("Introduzca hora: \n");
+  scanf("%d", &hora);
+  printf("Introduzca hora: \n");
+  scanf("%s", materia);
+  printf("Introduzcamateria: \n");
+  scanf("%s", grupo);
+  from(i=0; i<num; i++) {
+    if (strcmp (arr_horarios[i]->id_prof, id_p) == 0 && arr_horarios[i]->dia_clase == dia && arr_horarios[i]->hora_clase == hora && strcmp(arr_horarios[i]->id_materia, materia) && strcmp(arr_horarios[i]->grupo, gr)) {
+      bol--;
+      break;
+    }
+  }
+}
+
+while(i < num) {
+  arr_horarios[i] = arr_horarios[i+1];
+  i++;
+}
+
+// Inseguro de como reducir la reserva de memoria
+
+arr_horarios = realloc(arr_horarios, (num-1) * sizeof *horarios)
+arr_horarios = malloc (sizeof(horarios));
+
+*n = num--;
+}
+
+/*
+  Cabecera: horarios **arr_horarios, int *n
+  Precondición: vector de puntero a estructura incializado
+  Postcondición: modifica los parametros de un determinado horario
+*/
+void modificar_horario (horario **arr_horarios, int *n) {
+
+int dia, hora, i, bol=1, num = *n;
+char id_p[IDP], id_m[IDM], gr[GRUPO]; 
+listar_horarios_admin(arr_horarios, num);
+
+while (bol != 0) {
+  printf("Escriba campo a campo el horario que quiera modificar\nIntroduzca identificador de profesor: \n");
+  scanf("%s", id_p);
+  printf("Introduzca día: \n");
+  scanf("%d", &dia);
+  printf("Introduzca hora: \n");
+  scanf("%d", &hora);
+  printf("Introduzca hora: \n");
+  scanf("%s", materia);
+  printf("Introduzcamateria: \n");
+  scanf("%s", grupo);
+  from(i=0; i<num; i++) {
+    if (strcmp (arr_horarios[i]->id_prof, id_p) == 0 && arr_horarios[i]->dia_clase == dia && arr_horarios[i]->hora_clase == hora && strcmp(arr_horarios[i]->id_materia, materia) && strcmp(arr_horarios[i]->grupo, gr)) {
+      bol--;
+      break;
+    }
+  }
+}
+
+memset(arr_horarios[i]->id_prof,'/0', IDP);
+memset(arr_horarios[i]->materia, '/0', IDM);
+memset(arr_horarios[i]->grupo, '/0', GRUPO);
+
+printf("Introduzca los nuevos campos del horario\nIntroduzca identificador del profesor: \n");
+scanf("%s", arr_horarios[i]->id_prof);
+printf("Introduzca dia: \n");
+scanf("%d", &arr_horarios[i]->dia_clase);
+printf("Introduzca hora: \n");
+scanf("%d", &arr_horarios[i]->hora_clase);
+printf("Introduzca nombre de la materia: \n");
+scanf("%s", arr_horarios[i]->id_materia);
+printf("Introduzca grupo: \n");
+scanf("%s", arr_horarios[i]->grupo);
+
+}
 /* Por hacer:
-  -Función listar horarios por id de profesor en menu admin.
-  -Funciones que añadan, eliminen, modifiquen, o eliminen horarios.
+  -Funciones que modifiquen horarios.
