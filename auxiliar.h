b86b0b34e8ac4bf8518e3f904e4544be14972258
guilogar <jdkdejava@gmail.com>
2017-04-07 17:23:40 +0200

    Mejoras sustanciales. Creación de la función 'convertir_digito_cadena', que convierte un dígito en una cadena con tantos ceros a la derecha como se desee. También se ha reducido sustancialmente el número de líneas así como la complejidad del propio modulo.

--- auxiliar.h
+++ auxiliar.h
@@ -1,9 +1,10 @@
 #ifndef AUXILIAR_H
 #define AUXILIAR_H
 
-int get_line(char *prmpt, char *buff, size_t sz);
 int cuenta_lineas(char * nombre_fichero);
 int fecha_correcta(char * fecha);
 void str_replace(char *target, const char *needle, const char *replacement);
+void convertir_digito_cadena(char * target, char * longitud_cadena, int numero);
+int get_line(char *prmpt, char *buff, size_t sz);
 
 #endif
--- calificaciones.h
+++ calificaciones.h
@@ -5,7 +5,7 @@
 void cargar_calificaciones (FILE * f, calificaciones *list_calificaciones);
 void volcado_calificaciones(FILE * f, calificaciones *list_calificaciones, char *nom_file, int longitud_array);
 
-void menu_calificaciones(calificaciones *list_calificaciones, int *array_datos, int longitud_array);
+calificaciones * menu_calificaciones(calificaciones *list_calificaciones, int *array_datos, int longitud_array);
 
 calificaciones * anadir_calificacion   (calificaciones ** list_calificaciones, char datos[][100], int *longitud_array);
 void             modificar_calificacion(calificaciones *  list_calificaciones, char datos[][100], int  longitud_array);
@@ -18,7 +18,7 @@ void print_calificaciones_criba(calificaciones *list_calificaciones, int *array_
 void buscar_por_materia(calificaciones *list_calificaciones, int id_materia, calificaciones *new_list_calificaciones, int longitud_array);
 void buscar_por_alumno (calificaciones *list_calificaciones, int id_alumno,  calificaciones *new_list_calificaciones, int longitud_array);
 
-int get_longitud_array_calificaciones();
-int get_line(char *prmpt, char *buff, size_t sz);
 int nota_valida(int nota, char *msg_error);
+int get_longitud_array_calificaciones();
+
 #endif
