783f9a56c67cdc41ed5a883b95830808320150ac
guilogar <jdkdejava@gmail.com>
2017-04-17 19:00:16 +0200

    Mejoras en auxiliar.c, detecta el sistema operativo y actua en consecuencia

--- auxiliar.c
+++ auxiliar.c
@@ -2,8 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/*#include <regex.h>*/
 #include "auxiliar.h"
+
+#ifdef __unix__
+    #include <regex.h>
+#elif defined(_WIN32) || defined(WIN32)
+    // Estoy triste porque utilizas windows, en linux esto iria re-bien :(
+#endif
 
 int get_line (char *prmpt, char *buff, size_t sz) {
     int ch, extra;
@@ -39,7 +44,7 @@ void str_replace(char *target, const char *needle, const char *replacement){
     const char *tmp = target;
     size_t needle_len = strlen(needle);
     size_t repl_len = strlen(replacement);
-
+    
     while (1) {
         const char *p = strstr(tmp, needle);
         if (p == NULL) {
@@ -56,28 +61,31 @@ void str_replace(char *target, const char *needle, const char *replacement){
 }
 
 int fecha_correcta(char * fecha) {
-    return 1;
-/*
- *    regex_t regex;
- *    
- *    int reti;
- *    char msgbuf[100];
- *
- *    reti = regcomp(&regex, "[0-9][0-9][/-][0-9][0-9][/-][0-9][0-9][0-9][0-9]", 0);
- *    if (reti) {
- *        fprintf(stderr, "Could not compile regex\n");
- *    }
- *    reti = regexec(&regex, fecha, 0, NULL, 0);
- *    if (!reti) { return 1; }
- *    else if (reti == REG_NOMATCH) {
- *        puts("Esta fecha no puede ser valida, intentelo de nuevo.");
- *    }
- *    else {
- *        regerror(reti, &regex, msgbuf, sizeof(msgbuf));
- *        fprintf(stderr, "Regex match failed: %s\n", msgbuf);
- *    }
- *    return 0;
- */
+    
+    #ifdef __unix__
+        regex_t regex;
+        
+        int reti;
+        char msgbuf[100];
+        
+        reti = regcomp(&regex, "[0-9][0-9][/-][0-9][0-9][/-][0-9][0-9][0-9][0-9]", 0);
+        if (reti) {
+            fprintf(stderr, "Could not compile regex\n");
+        }
+        reti = regexec(&regex, fecha, 0, NULL, 0);
+        if (!reti) { return 1; }
+        else if (reti == REG_NOMATCH) {
+            puts("Esta fecha no puede ser valida, intentelo de nuevo.");
+        }
+        else {
+            regerror(reti, &regex, msgbuf, sizeof(msgbuf));
+            fprintf(stderr, "Regex match failed: %s\n", msgbuf);
+        }
+        return 0;
+    #elif defined(_WIN32) || defined(WIN32)
+        // Sorry, pero pa el windows yo no me complico.
+        return 1;
+    #endif
 }
 
 // Devuelve el numero de lienas en un fichero de texto.
